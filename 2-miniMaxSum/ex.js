993c17c7140829caa6239752c1e41da10fb93eaa
danieldificil <deasking.tc@outlook.com>
2022-03-21 16:41:19 -0300

    adding solution
    Solution for the request

--- 2-miniMaxSum/ex.js
+++ 2-miniMaxSum/ex.js
@@ -20,14 +20,14 @@ function readLine() {
     return inputString[currentLine++];
 }
 
-/*
- * Complete the 'miniMaxSum' function below.
- *
- * The function accepts INTEGER_ARRAY arr as parameter.
- */
-
-function miniMaxSum(arr) {
-// Write your code here
+function miniMaxSum(arr) 
+{
+    let Arr = arr
+    let n = arr.length
+    let sorted = Arr.sort((a, b) => a - b)
+    let sumMini = sorted[0] + sorted[1] + sorted[2] + sorted[3]
+    let sumMax = sorted[n-1] + sorted[n-2] + sorted[n-3] + sorted[n-4]
+    console.log(`${sumMini} ${sumMax}`) 
 }
 
 function main() {
