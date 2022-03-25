d0bbfc6ff472423fe5daa943f1784cca8e4447e8
danieldificil <deasking.tc@outlook.com>
2022-03-25 11:33:49 -0300

    Adding solution #003
    leaving only the function.

--- 3-timeConversion/ex.js
+++ 3-timeConversion/ex.js
@@ -1,27 +1,4 @@
-'use strict';
-
-const fs = require('fs');
-
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
-let inputString = '';
-let currentLine = 0;
-
-process.stdin.on('data', function(inputStdin) {
-    inputString += inputStdin;
-});
-
-process.stdin.on('end', function() {
-    inputString = inputString.split('\n');
-
-    main();
-});
-
-function readLine() {
-    return inputString[currentLine++];
-}
-
+//Function for time converte (AM/PM) for military model (24 h).
 function timeConversion(s)
 {
     let time = s
@@ -64,16 +41,4 @@ function timeConversion(s)
             break
         }        
     }
-}
-
-function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
-
-    const s = readLine();
-
-    const result = timeConversion(s);
-
-    ws.write(result + '\n');
-
-    ws.end();
 }
